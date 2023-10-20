0b1cefe5e5fba4de2dc56b472ffbe774f9f7deda
19SERGIO <sergiolondono55@gmail.com>
2023-10-20 00:43:32 -0500

    primeros

---
+++ primeros/ejer-17.py
@@ -0,0 +1,7 @@
+"""
+Capitalizar la primera letra de cada palabra en una cadena.
+    Función: `title()`
+
+"""
+cadena=input("ingrese un texto: ")
+print(cadena.title())
---
+++ primeros/ejer-18.py
@@ -0,0 +1,8 @@
+"""
+Contar la cantidad de palabras en una cadena.
+    Funciones: `split()` y `len()`
+
+"""
+texto=input("ingrese una cadena de texto: ")
+print(len(texto))
+print(texto.split())
