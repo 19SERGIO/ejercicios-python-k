019a931acd0d38cd8b785bb6102db16bc483e605
19SERGIO <sergiolondono55@gmail.com>
2023-10-20 00:05:16 -0500

    primeros ejercicios

---
+++ ejer-10.py
@@ -0,0 +1,12 @@
+"""
+Eliminar espacios en blanco al principio y al final de una cadena.
+    Función: `strip()`
+
+"""
+cadena=input("ingresa cualquier palabra dejando espacios antes de escribirla: ")
+print("sin strip se veria asi : " )
+print(cadena)
+
+resultado=cadena.strip()
+print("con strip se veria asi : " )
+print(resultado)
---
+++ ejer-11.py
@@ -0,0 +1,8 @@
+"""
+Contar cuántas veces aparece un carácter en una cadena.
+    Función: `count()`
+
+"""
+cadena=input("ingrese un texto cualquiera: ")
+letra=input("ingrese la letra que desea que se cuente el numero de veces que se encuentra en la cadena: ")
+print(cadena.count(letra))
