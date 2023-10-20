c6b91a92a70d4fa694f571beee811d57bbc4b13e
19SERGIO <sergiolondono55@gmail.com>
2023-10-19 23:26:10 -0500

    primeros ejercicios

--- ejer-06.py
+++ ejer-06.py
@@ -3,3 +3,5 @@ Dividir una cadena en palabras.
    Función: `split()`
 
 """
+cadena=input("Ingrese una cadena de texto: ")
+print(cadena.split())
---
+++ ejer-07.py
@@ -0,0 +1,8 @@
+"""
+Unir una lista de palabras en una sola cadena.
+   Función: `join()`
+
+"""
+palabras = ['hola', 'wuaky', 'dia', 'programar']
+resultado =" "
+print(resultado.join(palabras))
